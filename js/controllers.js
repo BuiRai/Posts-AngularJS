140d423e0adeba3ccea188ba1341b1730b174cf4
BuiRai <buirai_naetle@hotmail.com>
2015-06-29 14:45:09 -0500

    Show users
    I have implemented how to show all the users in the home.html

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,12 @@ angular.module("FinalApp")
 .controller("MainController", function($scope, $resource){
 	/*URL de la API: http://jsonplaceholder.typicode.com/posts/:id*/
 	Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {id: "@id"});
+	User = $resource("http://jsonplaceholder.typicode.com/users/:id", {id: "@id"});
 
-	$scope.posts = Post.query();
 	// query() -> GET/posts -> Devuelve un arreglo de posts
+	//Obteniendo los posts
+	$scope.posts = Post.query();
+	//Obteniendo los usuarios
+	$scope.users = User.query();
+	
 });
