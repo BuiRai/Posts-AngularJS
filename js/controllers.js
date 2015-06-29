73f49a5dde34d016cdeea64e072f18160365ef8c
BuiRai <buirai_naetle@hotmail.com>
2015-06-29 14:31:04 -0500

    Show post
    I have implemented how to show posts in the home.html

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,8 @@
 angular.module("FinalApp")
-.controller("MainController", function($scope){
+.controller("MainController", function($scope, $resource){
+	/*URL de la API: http://jsonplaceholder.typicode.com/posts/:id*/
+	Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {id: "@id"});
 
+	$scope.posts = Post.query();
+	// query() -> GET/posts -> Devuelve un arreglo de posts
 });
